40e7263df9c1e47cebc2c41e0c178fde03cfb950
mmatt26 <mmatt26@lsu.edu>
2017-03-19 18:36:48 -0500

    added in shooting

---
+++ Prototypes/First Combined Prototype/Assets/bulletScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletScript : MonoBehaviour {
+	public GameObject bullet;
+	public Transform firePoint;
+
+
+	Rigidbody2D r2d;
+	private void Start()
+	{
+		r2d = GetComponent<Rigidbody2D>();
+
+	}
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Return))
+		{
+			Instantiate(bullet, firePoint.position, firePoint.rotation);
+		}
+	}
+
+
+}
---
+++ Prototypes/First Combined Prototype/Assets/bulletShot.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletShot : MonoBehaviour {
+
+	public float speed = 6;
+	Rigidbody2D r2d;
+
+	// Use this for initialization
+	void Start () {
+		r2d = GetComponent<Rigidbody2D>();
+		r2d.velocity = new Vector2(0, speed);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+	private void OnBecameInvisible()
+	{
+		Destroy(gameObject);
+	}
+}
