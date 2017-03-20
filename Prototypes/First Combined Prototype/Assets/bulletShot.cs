20517f69a7081851b6e08cd1aa0bb85823da14ce
mmatt26 <mmatt26@lsu.edu>
2017-03-19 23:56:47 -0500

    added script to destroy enemy upon bullet contact

--- Prototypes/First Combined Prototype/Assets/bulletShot.cs
+++ Prototypes/First Combined Prototype/Assets/bulletShot.cs
@@ -6,19 +6,29 @@ public class bulletShot : MonoBehaviour {
 
 	public float speed = 6;
 	Rigidbody2D r2d;
+	public GameObject enemyDeath;
 
 	// Use this for initialization
 	void Start () {
 		r2d = GetComponent<Rigidbody2D>();
-		r2d.velocity = new Vector2(0, speed);
+		r2d.velocity = new Vector2(speed, 0);
 	}
 
 	// Update is called once per frame
 	void Update () {
 
 	}
-	private void OnBecameInvisible()
+
+	void OnCollisionEnter2D(Collision2D other)
 	{
-		Destroy(gameObject);
+		
+		if (other.gameObject.tag == "Enemy") {
+			Destroy (other.gameObject);
+			Destroy (gameObject);
+		}
+
 	}
+
+
+
 }
