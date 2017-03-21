382bd61f8d34921bc396550f3ba10986be9e8d41
yaldaAlizadeh <yalda.fazlalizadeh62@gmail.com>
2017-03-20 23:14:32 -0500

    change the score cs
    change the score cs

--- Prototypes/Scoring/Assets/Scripts/getScoreForShoot.cs
+++ Prototypes/Scoring/Assets/Scripts/getScoreForShoot.cs
@@ -6,38 +6,42 @@ using UnityEngine.UI;
 public class getScoreForShoot : MonoBehaviour
 {
     Rigidbody2D rb;
-    private int count;
     public Text scoreCount;
     public Text winText;
     public Text finishText;
+    public Text count;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         scoreCount = GameObject.FindGameObjectWithTag("scoreCount").GetComponent<Text>();
         winText = GameObject.FindGameObjectWithTag("winText").GetComponent<Text>();
-        finishText = GameObject.FindGameObjectWithTag("finishText").GetComponent<Text>();
-        count = 0;
+        finishText = GameObject.FindGameObjectWithTag("finishText").GetComponent<Text>(); 
+        count = finishText = GameObject.FindGameObjectWithTag("finishText").GetComponent<Text>();
         SetCountText();
         winText.text = "";
         finishText.text = "";
     }
 
 
-    void OnTriggerEnter2D(Collider2D other)
+    void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.tag == "Enemy") //shootin a microbe or parasite
         {
             other.gameObject.SetActive(false);
-            count = count + 10;
+            int value = int.Parse(count.text) + 10;
+            count.text = value.ToString();
+            //int anInteger;
+            //anInteger = Convert.ToInt32(textBox1.Text);
+            //anInteger = int.Parse(textBox1.Text);
             SetCountText();
         }
 
         else if (other.gameObject.CompareTag("Useful")) //shooting red or white blood cells by mistake
         {
             other.gameObject.SetActive(false);
-            count = count - 5;
-            SetCountText();
+            int value = int.Parse(count.text) - 5;
+            count.text = value.ToString();
         }
 
     }
