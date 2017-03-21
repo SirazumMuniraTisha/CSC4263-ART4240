308db544c64410975b9ffab2aaa7b9b3fd3fa76d
SirazumMuniraTisha <tisha.ruet09@gmail.com>
2017-03-20 19:55:02 -0500

    Added Boundary for movement

--- Prototypes/Moving Around/Assets/Scripts/Movement.cs
+++ Prototypes/Moving Around/Assets/Scripts/Movement.cs
@@ -1,20 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, yMin, yMax;
+}
 
-public class Movement : MonoBehaviour {
 
-    public float speed = 1.5f;
+public class Movement : MonoBehaviour
+{
+
+    public float speed = 10.0f;
+    public Boundary boundary;
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            transform.position += Vector3.left * speed * Time.deltaTime;
-        }
+
         if (Input.GetKey(KeyCode.RightArrow))
         {
+            
             transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            
+            transform.position += Vector3.left * speed * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.UpArrow))
         {
@@ -24,5 +35,15 @@ public class Movement : MonoBehaviour {
         {
             transform.position += Vector3.down * speed * Time.deltaTime;
         }
+        transform.position = new Vector3
+       (
+           Mathf.Clamp(transform.position.x, boundary.xMin, boundary.xMax),
+           Mathf.Clamp(transform.position.y, boundary.yMin, boundary.yMax),
+           0.0f
+
+       );
     }
+
+
 }
+
