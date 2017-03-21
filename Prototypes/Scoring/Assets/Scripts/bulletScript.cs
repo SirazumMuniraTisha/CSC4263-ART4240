a772aecf8f831502232ff1109b0221c4ae391c6e
yaldaAlizadeh <yalda.fazlalizadeh62@gmail.com>
2017-03-20 20:08:52 -0500

    new branch for scoring
    score system

--- Prototypes/Scoring/Assets/Scripts/bulletScript.cs
+++ Prototypes/Scoring/Assets/Scripts/bulletScript.cs
@@ -7,8 +7,12 @@ public class bulletScript : MonoBehaviour {
 
 	public GameObject bullet;
 	public Transform firePoint;
+	public Transform firePointL;
 	SpriteRenderer Capsule;
-	public Sprite CapsuleShot;
+	public Sprite CapsuleLeft;
+	public Sprite CapsuleRight;
+	public Sprite CapsuleShotL;
+	public Sprite CapsuleShotR;
 	Sprite init;
 	float dt1;
 	void Start()
@@ -19,16 +23,30 @@ public class bulletScript : MonoBehaviour {
 
 	void Update()
 	{
-		if (Input.GetKeyDown(KeyCode.Return))
+		if (Input.GetKeyDown(KeyCode.Space))
 		{
 			dt1 = DateTime.Now.Second;
-			Instantiate(bullet, firePoint.position, firePoint.rotation);
-			Capsule.sprite = CapsuleShot;
+
+			if (Capsule.sprite == CapsuleLeft) {
+				Capsule.sprite = CapsuleShotL;
+				Instantiate(bullet, firePointL.position, new Quaternion(0,0,180,0));
+			}
+			if (Capsule.sprite == CapsuleRight) {
+				Capsule.sprite = CapsuleShotR;
+				Instantiate(bullet, firePoint.position, new Quaternion(0,0,0,0));
+			}
+			//Capsule.sprite = CapsuleShot;
 			GetComponent<AudioSource> ().Play ();
 
 		}
 		if (DateTime.Now.Second - dt1 > 0.1f) {
-			Capsule.sprite = init;
+			if (Capsule.sprite == CapsuleShotL) {
+				Capsule.sprite = CapsuleLeft;
+			}
+			if (Capsule.sprite == CapsuleShotR) {
+				Capsule.sprite = CapsuleRight;
+			}
+			//Capsule.sprite = init;
 		}
 
 	}
