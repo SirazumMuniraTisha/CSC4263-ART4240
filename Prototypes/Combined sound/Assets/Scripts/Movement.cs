57d5ebd97a3ba77ad5422d067271d3fe7283f0fd
SirazumMuniraTisha <tisha.ruet09@gmail.com>
2017-03-20 19:40:51 -0500

    Added boundary to capi movement

--- Prototypes/Combined sound/Assets/Scripts/Movement.cs
+++ Prototypes/Combined sound/Assets/Scripts/Movement.cs
@@ -1,39 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, yMin, yMax;
+}
+
 
 public class Movement : MonoBehaviour {
 	SpriteRenderer Capsule;
 	Sprite CapsuleRight;
 	public Sprite CapsuleLeft;
 	public float speed = 10.0f;
+    public Boundary boundary;
 
-	void Start() {
+    void Start() {
 		Capsule = (SpriteRenderer)GetComponent("SpriteRenderer");
 		CapsuleRight = Capsule.sprite;
 	}
 	void Update () {
 
-		float x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
-		float y = Input.GetAxis("Vertical") * speed * Time.deltaTime;
-		transform.Translate (x, y, 0);
 		if(Input.GetKey(KeyCode.RightArrow)){
 			Capsule.sprite = CapsuleRight;
-		}
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
 		if (Input.GetKey (KeyCode.LeftArrow)) {
 			Capsule.sprite = CapsuleLeft;
-		}
-		//if (Input.GetKey (KeyCode.RightArrow)) {
-		//	transform.position += Vector3.right * speed * Time.deltaTime;
-		//	print ((float)(firePoint.position.x - Middle.position.x));
-		//Capsule.sprite = (Sprite) CapsuleRight;
-		//}
-		//if (Input.GetKey (KeyCode.UpArrow)) {
-		//	transform.position += Vector3.up * speed * Time.deltaTime;
-		//}
-		//if (Input.GetKey (KeyCode.DownArrow)) {
-		//	transform.position += Vector3.down * speed * Time.deltaTime;
-		//}
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += Vector3.up * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position += Vector3.down * speed * Time.deltaTime;
+        }
+        transform.position = new Vector3
+       (
+           Mathf.Clamp(transform.position.x, boundary.xMin, boundary.xMax),
+           Mathf.Clamp(transform.position.y, boundary.yMin, boundary.yMax),
+           0.0f
+           
+       );
+    }
 
-	}
-}
+       
+        //if (Input.GetKey (KeyCode.RightArrow)) {
+        //	transform.position += Vector3.right * speed * Time.deltaTime;
+        //	print ((float)(firePoint.position.x - Middle.position.x));
+        //Capsule.sprite = (Sprite) CapsuleRight;
+        //}
+        //if (Input.GetKey (KeyCode.UpArrow)) {
+        //	transform.position += Vector3.up * speed * Time.deltaTime;
+        //}
+        //if (Input.GetKey (KeyCode.DownArrow)) {
+        //	transform.position += Vector3.down * speed * Time.deltaTime;
+        //}
+
+    }
+
