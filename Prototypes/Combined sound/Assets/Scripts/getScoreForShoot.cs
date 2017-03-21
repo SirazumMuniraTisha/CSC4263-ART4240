53847e7a70e5a60eaa3ce2b02c24ecec2cd51b05
yaldaAlizadeh <yalda.fazlalizadeh62@gmail.com>
2017-03-21 02:20:13 -0500

    commit scoring changes
    commit scoring changes

--- Prototypes/Combined sound/Assets/Scripts/getScoreForShoot.cs
+++ Prototypes/Combined sound/Assets/Scripts/getScoreForShoot.cs
@@ -9,15 +9,14 @@ public class getScoreForShoot : MonoBehaviour
     public Text scoreCount;
     public Text winText;
     public Text finishText;
-    public Text count;
+    public static int count = 0;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         scoreCount = GameObject.FindGameObjectWithTag("scoreCount").GetComponent<Text>();
         winText = GameObject.FindGameObjectWithTag("winText").GetComponent<Text>();
-        finishText = GameObject.FindGameObjectWithTag("finishText").GetComponent<Text>(); 
-        count = finishText = GameObject.FindGameObjectWithTag("finishText").GetComponent<Text>();
+        finishText = GameObject.FindGameObjectWithTag("finishText").GetComponent<Text>();
         SetCountText();
         winText.text = "";
         finishText.text = "";
@@ -29,19 +28,16 @@ public class getScoreForShoot : MonoBehaviour
         if (other.gameObject.tag == "Enemy") //shootin a microbe or parasite
         {
             other.gameObject.SetActive(false);
-            int value = int.Parse(count.text) + 10;
-            count.text = value.ToString();
-            //int anInteger;
-            //anInteger = Convert.ToInt32(textBox1.Text);
-            //anInteger = int.Parse(textBox1.Text);
+            count = count + 10;
             SetCountText();
         }
 
         else if (other.gameObject.CompareTag("Useful")) //shooting red or white blood cells by mistake
         {
             other.gameObject.SetActive(false);
-            int value = int.Parse(count.text) - 5;
-            count.text = value.ToString();
+            count = count - 5;
+            SetCountText();
+
         }
 
     }
@@ -50,6 +46,18 @@ public class getScoreForShoot : MonoBehaviour
     {
         scoreCount.text = "Score: " + count.ToString();
         
+        if(count >= 20)
+        {
+            print(count);
+            //Vector2 initPosition = winText.transform.position;
+            //Vector2 currentPosition;
+            //currentPosition.y = initPosition.y - 600.0f;
+            //winText.transform.Translate(0, 0, 0);
+            winText.gameObject.SetActive(true);
+        }
+        
     }
+
+
 }
 
--- Prototypes/Combined sound/Assets/Scripts/initialScore.cs
+++ Prototypes/Combined sound/Assets/Scripts/initialScore.cs
@@ -9,10 +9,13 @@ public class initialScore : MonoBehaviour
     Rigidbody2D rb;
     private int count;
     public Text currentScoreText;
+    public Text winText;
 
     void Start()
     {
         currentScoreText = GetComponent<Text>();
+        winText = GameObject.FindGameObjectWithTag("winText").GetComponent<Text>();
+        winText.gameObject.SetActive(false);
         count = 0;
         //SetCountText();
         currentScoreText.text = "Score: " + count.ToString();
