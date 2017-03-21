8f8824b14ab7fb9692b69558a2ce05b233651e89
yaldaAlizadeh <yalda.fazlalizadeh62@gmail.com>
2017-03-21 04:50:48 -0500

    Add RemainingTime and GameOver and Game Win
    Add RemainingTime and GameOver and Game Win

---
+++ Prototypes/Combined sound/Assets/Scripts/GameWinAndGameOver.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameWinAndGameOver : MonoBehaviour {
+
+    public float timeIncrease = 2;
+    public bool timeElapsed = false;
+    public Text winText;
+    public Text finishText;
+    public float timeLeft = 30.0f;
+    public int itemsRemaining;
+
+    void Start () {
+        GameObject[] items = GameObject.FindGameObjectsWithTag("Enemy");
+        itemsRemaining = items.Length;
+        winText = GameObject.FindGameObjectWithTag("winText").GetComponent<Text>();
+        finishText = GameObject.FindGameObjectWithTag("finishText").GetComponent<Text>();
+
+    }
+
+    float currCountdownValue;
+    public IEnumerator StartCountdown(float countdownValue = 10)
+    {
+        currCountdownValue = countdownValue;
+        while (currCountdownValue > 0)
+        {
+            Debug.Log("Countdown: " + currCountdownValue);
+            yield return new WaitForSeconds(1.0f);
+            currCountdownValue--;
+        }
+    }
+
+
+    void Update () {
+        timeLeft -= Time.deltaTime;
+        if (timeLeft <= 0)
+        {
+            if (itemsRemaining == 0)
+            {
+                //you win
+                winText.text = "Congratulations! You Win.";
+                winText.gameObject.SetActive(true);
+                //Won this level: EndOfThisLevel()
+            }
+            else
+            {
+                //Game over
+                finishText.text = "Sorry, the game is over.";
+                finishText.gameObject.SetActive(true);
+                //Lost this level: EndOfThisLevel()
+            }
+        }
+        else if (itemsRemaining == 0)
+        {
+            //you win
+            winText.text = "Congratulations! You Win.";
+            winText.gameObject.SetActive(true);
+            //Won this level: EndOfThisLevel()
+        }
+    }
+
+
+}
--- Prototypes/Combined sound/Assets/Scripts/bulletShot.cs
+++ Prototypes/Combined sound/Assets/Scripts/bulletShot.cs
@@ -9,7 +9,9 @@ public class bulletShot: MonoBehaviour {
 	//public GameObject enemyDeath;
 	float initPosition;
 	float initRotation;
-	void Start() {
+    Text consultText;
+
+    void Start() {
 		initPosition = transform.position.x;
 		initRotation = transform.rotation.z;
 	}
@@ -22,18 +24,20 @@ public class bulletShot: MonoBehaviour {
 		if (initPosition - transform.position.x > 5 || initPosition - transform.position.x < -5) {
 			Destroy (gameObject);
 		}
-	}
+        consultText = GameObject.FindGameObjectWithTag("consult").GetComponent<Text>();
+        consultText.text = "";
+    }
 
 	void OnCollisionEnter2D(Collision2D other)
 	{
 		if (other.gameObject.tag == "Enemy") {
             Text immunityText = GameObject.FindGameObjectWithTag("immunity").GetComponent<Text>();
-            Text consultText = GameObject.FindGameObjectWithTag("consult").GetComponent<Text>();
+            consultText = GameObject.FindGameObjectWithTag("consult").GetComponent<Text>();
             int currentImmunity = int.Parse(immunityText.text.ToString().Substring(16, immunityText.text.ToString().IndexOf("%")-16)) - 5;
             //print(currentImmunity);
             immunityText.text = "Immunity Level: " + currentImmunity.ToString() + "%";
             consultText.text = "You get 10 scores but your immunity level decreased 5 points because of shooting.";
-            
+        
             Destroy(other.gameObject);
 			Destroy (gameObject);
 		}
