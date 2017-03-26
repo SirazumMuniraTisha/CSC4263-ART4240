9055254919ba5e485a982e29cf210f61b4e16602
mmatt26 <mmatt26@lsu.edu>
2017-03-26 09:26:07 -0500

    testing a different enemy movement
    temporary change

---
+++ Prototypes/Combined sound/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+	public float speed = 5f;
+	Rigidbody2D r2d;
+	// Use this for initialization
+	void Start () {
+		r2d = GetComponent<Rigidbody2D>();
+		float y = r2d.velocity.y;
+		y = speed;
+		r2d.angularVelocity = Random.Range (-10, 10);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	private void OnBecameInvisible()
+	{
+		Destroy(gameObject);
+	}
+}
