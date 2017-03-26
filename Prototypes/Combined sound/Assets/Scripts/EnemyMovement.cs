29ff7e59ec9090158423da64faabe44fae5ce077
mmatt26 <mmatt26@lsu.edu>
2017-03-26 14:23:32 -0500

    Protyping enemy movement towards player

--- Prototypes/Combined sound/Assets/Scripts/EnemyMovement.cs
+++ Prototypes/Combined sound/Assets/Scripts/EnemyMovement.cs
@@ -3,20 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour {
-	public float speed = 5f;
-	Rigidbody2D r2d;
+	public float speed = 8f;
+	//Rigidbody2D r2d;
+	private Transform target;
+//	public Transform enemy;
+	public float rotationSpeed = 3f;
+
 	// Use this for initialization
+
 	void Start () {
-		r2d = GetComponent<Rigidbody2D>();
-		float y = r2d.velocity.x;
-		y = speed;
-		r2d.angularVelocity = Random.Range (-.1f, .1f);
+
+		target = GameObject.FindGameObjectWithTag ("Player").transform;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		transform.position = Vector2.MoveTowards (transform.position, target.transform.position, speed * Time.deltaTime);
 	}
+
 	private void OnBecameInvisible()
 	{
 		Destroy(gameObject);
