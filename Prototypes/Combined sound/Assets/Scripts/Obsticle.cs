295f893613082cd04c2569dd3eb228e88b65f656
mmatt26 <mmatt26@lsu.edu>
2017-03-26 21:21:28 -0500

    slowed powerUp spawnRate. testing obsticle objects

---
+++ Prototypes/Combined sound/Assets/Scripts/Obsticle.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obsticle : MonoBehaviour {
+	Rigidbody2D obsticle;
+
+	// Use this for initialization
+	void Start () {
+		obsticle = gameObject.GetComponent<Rigidbody2D> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Prototypes/Combined sound/Assets/Scripts/PowerUpSpawn.cs
+++ Prototypes/Combined sound/Assets/Scripts/PowerUpSpawn.cs
@@ -4,21 +4,21 @@ using UnityEngine;
 /**
  * code to respawn enemies
  */
-public class SpawnScript : MonoBehaviour {
+public class PowerUpSpawn: MonoBehaviour {
 
-	public GameObject enemy; //enemy prefab to be spawned
-	public float spawnTime = 9f; //time between spawns
+	public GameObject power; //enemy prefab to be spawned
+	public float spawnTime = 15f; //time between spawns
 	public Transform [] spawnPoint;
 
 	void Start()
 	{
-		//Call spzwn function every 2 seconds
+		//Call spzwn function every 15 seconds
 		InvokeRepeating("Spawn", 15f, spawnTime);
 	}
 
 	void Spawn()
 	{
 		int spawnPointIndex = Random.Range (0, spawnPoint.Length);
-		Instantiate (enemy, spawnPoint [spawnPointIndex].position, spawnPoint [spawnPointIndex].rotation);
+		Instantiate (power, spawnPoint [spawnPointIndex].position, spawnPoint [spawnPointIndex].rotation);
 	}
 }
