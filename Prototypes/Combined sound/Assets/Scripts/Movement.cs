197491cde278aa41e7bac385844f8152716577c3
mmatt26 <mmatt26@lsu.edu>
2017-04-04 11:30:53 -0500

    debugging powerUps

--- Prototypes/Combined sound/Assets/Scripts/Movement.cs
+++ Prototypes/Combined sound/Assets/Scripts/Movement.cs
@@ -15,6 +15,7 @@ public class Movement : MonoBehaviour {
 	public float speed = 10.0f;
     public Boundary boundary;
 	public int health = 100;
+    public int count;
 
     void Start() {
 		Capsule = (SpriteRenderer)GetComponent("SpriteRenderer");
@@ -80,10 +81,16 @@ public class Movement : MonoBehaviour {
 	 */
 	void OnTriggerEnter2D(Collider2D other)
 	{
+        
 		if (other.gameObject.tag == "Useful") {
 			Debug.Log ("upgrade");
-			health += 5;
-			Destroy (other.gameObject);
+            if (health < 110)
+            {
+                health += 5;
+                Destroy(other.gameObject);
+                Debug.Log(health);
+            }
+
 		}
 	}
 
