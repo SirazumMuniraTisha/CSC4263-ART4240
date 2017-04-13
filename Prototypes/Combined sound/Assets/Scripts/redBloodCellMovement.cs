864cd62cfde60a15204b5cbeb1b9ff945343f93d
yaldaAlizadeh <yalda.fazlalizadeh62@gmail.com>
2017-04-13 17:29:05 -0500

    expand the path
    expand the path

---
+++ Prototypes/Combined sound/Assets/Scripts/redBloodCellMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class redBloodCellMovement : MonoBehaviour {
+
+    SpriteRenderer RedBloodCell;
+    public float speed = 10.0f;
+    public Boundary boundary;
+
+    void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
