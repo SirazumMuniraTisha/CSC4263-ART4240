a8e180ddf662fa4c4e924e00975f15a9644b53a0
SirazumMuniraTisha <tisha.ruet09@gmail.com>
2017-04-16 20:03:39 -0500

    Added hearts As Immunity

---
+++ Prototypes/Combined sound/Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+
+    public int curhealth;
+    public int maxhealth = 5;
+    void Start()
+    {
+        curhealth = 0;
+
+    }
+
+    void Update()
+    {
+        if (curhealth >= maxhealth)
+            curhealth = maxhealth;
+
+    }
+}
