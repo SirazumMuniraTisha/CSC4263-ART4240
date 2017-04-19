c59aad377f25d466dbbfba3c93249f6d255561fb
mmatt26 <mmatt26@lsu.edu>
2017-04-19 16:44:19 -0500

    added enemy shooting

---
+++ Prototypes/Combined sound/Assets/Scripts/EShot.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class EShot : MonoBehaviour {
+    public GameObject bullet;
+    public Transform firePoint;
+    SpriteRenderer BA3;
+    public Sprite BA1oclock;
+    public Sprite BA3oclock;
+    public Sprite BA5oclock;
+    public Sprite BA7oclock;
+    public Sprite BA9oclock;
+
+    Sprite init;
+    float dt1;
+    void Start()
+    {
+        BA3 = (SpriteRenderer)GetComponent("SpriteRenderer");
+        init = BA3.sprite;
+    }
+
+    void Update()
+    {
+        
+            dt1 = DateTime.Now.Second;
+        /**
+                    
+                    if (BA3.sprite == BA3oclock)
+                    {
+                        
+                        Instantiate(bullet, firePoint.position, new Quaternion(0, 0, 0, 0));
+                    }
+                    
+                    if (BA3.sprite == BA9oclock)
+                    {
+
+                        Instantiate(bullet, firePoint.position, new Quaternion(0, 0, 180, 0));
+                    }
+
+               */
+        Instantiate(bullet, firePoint.position, Quaternion.identity);
+    }
+}
---
+++ Prototypes/Combined sound/Assets/Scripts/WeaponScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponScript : MonoBehaviour {
+
+    public Transform enemyBullet;
+    //public float shootRate = 0.25f;
+    //private float shootCoolDown;
+
+
+    // Use this for initialization
+    public float speed = 6;
+    //public GameObject enemyDeath;
+    float initPosition;
+    float initRotation;
+   
+    void Start()
+    {
+        initPosition = transform.position.x;
+        initRotation = transform.rotation.z;
+    }
+    void Update()
+    {
+        Vector3 move = Vector2.right * Time.deltaTime * speed;
+        if (initRotation == 180)
+        {
+            move = (-1) * move;
+        }
+        transform.Translate(move);
+        if (initPosition - transform.position.x > 5 || initPosition - transform.position.x < -5)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+
+    // Instantiate(enemyBullet, transform.position, Quaternion.identity);
+}
