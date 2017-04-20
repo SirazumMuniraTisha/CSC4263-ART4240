b7c0d019248968651b1e987324a4d7099101189b
Iris Chen <candyfairest@hotmail.com>
2017-04-20 07:06:43 -0500

    Add welcome scene and connect to game

---
+++ Prototypes/Combined sound/Assets/Scripts/SceneChange.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneChange : MonoBehaviour {
+
+	public void Change (string SceneName){
+		Application.LoadLevel(SceneName);
+	}
+
+}
