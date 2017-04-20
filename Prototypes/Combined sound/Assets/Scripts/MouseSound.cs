f52e9bd49c401eb2e5f3ac5b67fcb20619744d64
Iris Chen <candyfairest@hotmail.com>
2017-04-20 15:21:19 -0500

    BackgroundSound in 2 scene, modify button size

---
+++ Prototypes/Combined sound/Assets/Scripts/MouseSound.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MouseSound : MonoBehaviour {
+	public AudioClip Sound;
+	public AudioSource Source;
+	void Start()
+	{
+		Source = GetComponent<AudioSource>();
+	}
+	void OnMouseEnter(){
+		Debug.Log ("Play_Audio");
+		Source.PlayOneShot (Sound);
+	}
+}
