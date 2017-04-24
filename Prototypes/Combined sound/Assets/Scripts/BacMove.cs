c7f20b64b10a1113f6ed48c39d5498b3ca5850bd
Iris Chen <candyfairest@hotmail.com>
2017-04-24 02:49:16 -0500

    Welcome anim, game script(bacmove) merge

---
+++ Prototypes/Combined sound/Assets/Scripts/BacMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BacMove : MonoBehaviour {
+	public float times;
+	public int direction; //x=1, y=2
+	Rigidbody2D rigidBody;
+	Vector2 currentPosition, initPosition;
+	float curr, init;
+	void Start () {
+		rigidBody = gameObject.GetComponent<Rigidbody2D> ();
+		initPosition = gameObject.transform.position;
+	}
+
+	void Update () {
+		currentPosition = initPosition;
+		if (direction == 1)
+			currentPosition.x = initPosition.x + Mathf.Sin (Time.timeSinceLevelLoad)*times;
+		else if (direction == 2)
+			currentPosition.y = initPosition.y + Mathf.Sin (Time.timeSinceLevelLoad)*times;
+		rigidBody.MovePosition (currentPosition);
+	}
+}
