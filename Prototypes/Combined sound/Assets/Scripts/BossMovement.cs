3457e32e9be095032aa4839abd34d9112c7c9f93
SirazumMuniraTisha <tisha.ruet09@gmail.com>
2017-04-24 18:07:19 -0500

    Added boss and its movement

---
+++ Prototypes/Combined sound/Assets/Scripts/BossMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossMovement : MonoBehaviour {
+
+    Rigidbody2D rigidBody;
+    Vector2 currentPosition;
+    Vector2 initialPosition;
+
+    void Start()
+    {
+        rigidBody = gameObject.GetComponent<Rigidbody2D>();
+        initialPosition = gameObject.transform.position;
+    }
+
+
+    void Update()
+    {
+        currentPosition = initialPosition;
+        currentPosition.x = initialPosition.x + Mathf.Sin(2*Time.timeSinceLevelLoad);
+        currentPosition.y = initialPosition.y + Mathf.Sin( Time.timeSinceLevelLoad);
+        rigidBody.MovePosition(currentPosition);
+    }
+}
