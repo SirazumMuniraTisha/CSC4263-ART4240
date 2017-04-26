804e7ecf74f68155dcd5bf7b064989b4b18a89db
SirazumMuniraTisha <tisha.ruet09@gmail.com>
2017-04-26 10:46:52 -0500

    Thinking about boss bullets as the microbe

---
+++ Prototypes/Combined sound/Assets/Scripts/BossFightWeapon.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossFightWeapon : MonoBehaviour {
+
+    public GameObject bullet;
+    public Transform[] firePoint;
+    // public Transform firePoint2;
+    //public Transform firePoint1;
+    //  public Transform firePoint1;
+
+
+    GameObject temp;
+    Sprite init;
+    float dt1;
+    private float fireRate = 1f;
+    private float range = 0.5f;
+    private Transform target;
+
+    private float nextFire;
+    private Transform boss;
+    private float timer;
+
+    void Start()
+    {
+
+        if (GameObject.FindGameObjectWithTag("Player") != null)
+            target = GameObject.FindGameObjectWithTag("Player").transform;
+
+
+
+    }
+
+    void Update()
+    {
+
+        Invoke("ShootBull", 0.5f);
+
+    }
+
+    void ShootBull()
+    {
+        if (target != null)
+        {
+            //boss = GameObject.FindGameObjectWithTag("Boss").transform;
+            float distance = Vector2.Distance(transform.position, target.position);
+
+            if (Time.time > nextFire)
+            {
+                nextFire = Time.time + fireRate;
+
+                if (distance <= range)
+                {
+                    // = DateTime.Now.Second;
+                    //  nextFire = Time.time + fireRate;
+                    int firePointIndex = Random.Range(0, firePoint.Length);
+                    Instantiate(bullet, firePoint[firePointIndex].position, firePoint[firePointIndex].rotation);
+
+                    //Instantiate(bullet, firePoint1.position, firePoint1.rotation);
+                    //Instantiate(bullet, firePoint2.position, firePoint2.rotation);
+                    //Instantiate(bullet, firePoint3.position, firePoint3.rotation);
+                }
+
+                GameObject instance = Instantiate(bullet);
+            }
+        }
+    }
+}
