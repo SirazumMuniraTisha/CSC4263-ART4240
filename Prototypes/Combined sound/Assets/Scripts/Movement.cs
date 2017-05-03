0734e916eef8069ed0f28b3b3af6c1ca52d79cf5
SirazumMuniraTisha <tisha.ruet09@gmail.com>
2017-05-03 14:03:21 -0500

    if health is less than 5 then no sheild

--- Prototypes/Combined sound/Assets/Scripts/Movement.cs
+++ Prototypes/Combined sound/Assets/Scripts/Movement.cs
@@ -28,15 +28,15 @@ public class Movement : MonoBehaviour {
 		gameObject.transform.rotation = initRotation;
 		if (health > 0) {
 			if (Input.GetKey (KeyCode.RightArrow)) {
-				if (player.curhealth == 0)
+				if (player.curhealth == 0 || health < 5)
 					Capsule.sprite = CapsuleRight;
 				else
 					Capsule.sprite = ShieldCapiRight;
 
-				transform.position += Vector3.right * speed * Time.deltaTime;
+                transform.position += Vector3.right * speed * Time.deltaTime;
 			}
 			if (Input.GetKey (KeyCode.LeftArrow)) {
-				if (player.curhealth == 0)
+				if (player.curhealth == 0 || health < 5)
 					Capsule.sprite = CapsuleLeft;
 				else
 					Capsule.sprite = ShieldCapiLeft;
